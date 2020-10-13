45c85763692eef34b348b4f08db2eae9881f761b
Chen H <vamdawn@outlook.com>
2020-10-14 02:52:28 +0800

    feat(utils): add HttpUtils

---
+++ src/main/java/chen/binflare/utils/HttpUtils.java
@@ -0,0 +1,11 @@
+package chen.binflare.utils;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * okhttp3 工具封装
+ */
+public class HttpUtils {
+
+    public static final OkHttpClient CLIENT = new OkHttpClient();
+}
