9202bb3b86b79c13c4dc58e91644f4ea6d52d3f1
Chen_H <vamdawn@outlook.com>
2020-12-06 00:00:46 +0800

    feat(config): add config constants

---
+++ src/main/java/chen/binflare/constant/ConfigConstants.java
@@ -0,0 +1,10 @@
+package chen.binflare.constant;
+
+/**
+ * a little configuration constants
+ */
+public class ConfigConstants {
+
+    public static final String LOGIN_PUBLIC_KEY = "LOGIN_PUBLIC_KEY";
+    public static final String LOGIN_PRIVATE_KEY = "LOGIN_PRIVATE_KEY";
+}
