6a0fd1d39fbe415a0b0fa23e3d5df145e6df4a95
Chen_H <vamdawn@outlook.com>
2020-12-06 00:04:45 +0800

    feat(config): add crypto constants

---
+++ src/main/java/chen/binflare/constant/CryptoConstants.java
@@ -0,0 +1,10 @@
+package chen.binflare.constant;
+
+/**
+ * about crypto constants
+ */
+public class CryptoConstants {
+
+    public static final String LOGIN_KEY_ALGORITHM = "RSA";
+    public static final int LOGIN_KEY_SIZE = 1024;
+}
