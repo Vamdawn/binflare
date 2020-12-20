be020f7fa50156ed206987197e81204d8bac8ac2
Chen H <vamdawn@outlook.com>
2020-12-20 22:49:49 +0800

    feat(dependency): add constraints on CreateTokenReqDTO

--- src/main/java/chen/binflare/dto/users/CreateTokenReqDTO.java
+++ src/main/java/chen/binflare/dto/users/CreateTokenReqDTO.java
@@ -4,18 +4,21 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 @ApiModel(description = "获取访问凭据请求")
 public class CreateTokenReqDTO implements Serializable {
 
+    @NotNull
     @ApiModelProperty(value = "鉴权用户名")
     private String userName;
 
     @ApiModelProperty(value = "鉴权密码")
     private String passWord;
 
+    @NotNull
     @ApiModelProperty(value = "鉴权账号类型")
     private Byte userType;
 
