692529e8f4d1274f0418e425fe3514a11a3b4f66
Chen H <vamdawn@outlook.com>
2020-12-21 17:14:26 +0800

    refactor(dao): rename `user_credential` column `type` and `value`

--- src/main/java/chen/binflare/service/impl/UsersServiceImpl.java
+++ src/main/java/chen/binflare/service/impl/UsersServiceImpl.java
@@ -1,13 +1,18 @@
 package chen.binflare.service.impl;
 
+import chen.binflare.dao.UserCredentialDAO;
 import chen.binflare.dto.users.CreateTokenRespDTO;
 import chen.binflare.service.UsersService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class UsersServiceImpl implements UsersService {
+
+    @Autowired
+    private UserCredentialDAO userCredentialDAO;
 
     @Override
     public CreateTokenRespDTO createAccessToken(String userName, String passWord, Integer userType, Integer passType) {
