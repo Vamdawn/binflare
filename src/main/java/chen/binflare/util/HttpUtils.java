b6d535ce50b64847e150025d58dbfa188e7f099b
Chen H <vamdawn@outlook.com>
2020-12-24 00:14:24 +0800

    feat(util): add HTTP Methods in HttpUtils

--- src/main/java/chen/binflare/util/HttpUtils.java
+++ src/main/java/chen/binflare/util/HttpUtils.java
@@ -8,4 +8,11 @@ import okhttp3.OkHttpClient;
 public class HttpUtils {
 
     public static final OkHttpClient CLIENT = new OkHttpClient();
+
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String PATCH = "PATCH";
+    public static final String DELETE = "DELETE";
+    public static final String OPTION = "OPTION";
 }
