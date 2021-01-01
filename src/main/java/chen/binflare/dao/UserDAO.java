b6b32776335d4354ee4e5d7bd19582d1d0f841fc
Chen H <vamdawn@outlook.com>
2021-01-02 00:11:51 +0800

    REFACTOR(DO): change table `user` ddl

--- src/main/java/chen/binflare/dao/UserDAO.java
+++ src/main/java/chen/binflare/dao/UserDAO.java
@@ -3,5 +3,5 @@ package chen.binflare.dao;
 import chen.binflare.entity.UserDO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDAO extends JpaRepository<UserDO, Integer> {
+public interface UserDAO extends JpaRepository<UserDO, Long> {
 }
--- src/main/java/chen/binflare/entity/UserDO.java
+++ src/main/java/chen/binflare/entity/UserDO.java
@@ -13,19 +13,16 @@ public class UserDO {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private Long id;
 
-    @Column(name = "account_no")
-    private String accountNo;
+    @Column(name = "status")
+    private Byte status;
 
     @Column(name = "nick_name")
     private String nickName;
 
     @Column(name = "avatar")
     private String avatar;
-
-    @Column(name = "token")
-    private String token;
 
     @Column(name = "create_time")
     private Instant createTime;
